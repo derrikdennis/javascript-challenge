ef4261d62840454415f5168ac83ddc519cffd79e
Derrik Dennis <derrik@austin.rr.com>
2020-08-15 15:51:39 -0500

    Finalized changes for homework

--- static/js/app.js
+++ static/js/app.js
@@ -3,31 +3,78 @@ var tableData = data;
 
 
 // YOUR CODE HERE!
+//Capturing the two pieces of the HTML we will need.
 var tbody = d3.select("tbody");
 
 var button = d3.select("#filter-btn");
 
-button.on("click",handleclick);
+
+// Function that will handle what happens when you FilterTable Button.
 
 function handleclick(){
-    var inputField = d3.select('.form-control');
-    console.log("a button was clicked!");
-    console.log(d3.event.target);
-    console.log(`the input field is: ${inputField}`);
-}
 
-//inputField.on("change", function() {
-//    var newText = d3.event.target.value;
-//    console.log(newText);
-//});
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
 
-//var filterDate = data.filter(function(UFOData){
-//    return UFOData.datetime === newText;
-//});
+    //Looks at the values that are added to the filter.
 
-data.forEach(function(UFOData){
-    var row = tbody.append("tr");
-    Object.values(UFOData).forEach(function(value){
-        row.append("td").text(value);
+    let date = d3.select('#datetime').property("value");
+    let city = d3.select('#city').property("value");
+    let state = d3.select('#state').property("value");
+    let country = d3.select('#country').property("value");
+    let shape = d3.select('#shape').property("value");
+    
+    //Creates a variable that will take our data and run it through our filters.
+    let filterDate = data;
+
+    //This is our filters
+    if (date){
+        filterDate = filterDate.filter((UFOData) => {
+            return UFOData.datetime == date;
+        });
+    
+    };
+    if (city){
+        filterDate = filterDate.filter((UFOData) => {
+            return UFOData.city == city;
+        });
+    
+    };
+    if (state){
+        filterDate = filterDate.filter((UFOData) => {
+            return UFOData.state == state;
+        });
+    
+    };
+    if (country){
+        filterDate = filterDate.filter((UFOData) => {
+            return UFOData.country == country;
+        });
+    
+    };
+    if (shape){
+        filterDate = filterDate.filter((UFOData) => {
+            return UFOData.shape == shape;
+        });
+    
+    };
+
+    //This will refresh the page and not just paste the data to the end of the table.
+    tbody.html("")
+    buildTable(filterDate);
+};
+
+//Function to build the table.
+function buildTable(data) {
+    data.forEach((UFOData) => {
+        var row = tbody.append("tr");
+        Object.values(UFOData).forEach((value) => {
+            row.append("td").text(value);
+        });
     });
-});
+};
+
+buildTable(tableData);
+
+//Clicking on the button will allow you to filter the data.
+button.on("click",handleclick);
